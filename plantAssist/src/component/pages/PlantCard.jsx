ffe3839b9854884a949d657ef8e03e9da381fcba
madmeche <madeleine.meche@gmail.com>
2023-08-26 12:46:31 -0500

    adding multiple favorites

--- plantAssist/src/component/pages/PlantCard.jsx
+++ plantAssist/src/component/pages/PlantCard.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { ReactDOM } from "react";
+import { json } from "react-router-dom";
 
 function PlantCard(props) {
-    const {plant} = props;
+  const { plant } = props;
 
-    const handleFavorite = () => {
-        console.log(plant.id)
-
-        fetch("http://localhost:8080/api/plant/favorites")
-        .then((response) => response.json())
-        .then((json) => {
-          console.log("Response", json.data);
-          setData(json.data);
-        });
-   
-
+  const handleFavorite = () => {
+    console.log(plant.id);
+    const userId = sessionStorage.getItem("id");
+    const data = {
+        plantId: plant.id
     }
-
-    
+    fetch(`http://localhost:8080/api/favorites/${userId}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",      
+          },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("Response", json);
+      });
+  };
 
   return (
     <>
-     
       <h1>Plant Cards Here</h1>
       <div>Name: {plant.name}</div>
       <div>Description: {plant.description}</div>
