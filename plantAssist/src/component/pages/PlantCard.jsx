46076080ae4e72095083ce025d52880db977ac99
madmeche <madeleine.meche@gmail.com>
2023-08-26 12:28:27 -0500

    created favorite id for user

--- plantAssist/src/component/pages/PlantCard.jsx
+++ plantAssist/src/component/pages/PlantCard.jsx
@@ -3,6 +3,22 @@ import { ReactDOM } from "react";
 
 function PlantCard(props) {
     const {plant} = props;
+
+    const handleFavorite = () => {
+        console.log(plant.id)
+
+        fetch("http://localhost:8080/api/plant/favorites")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log("Response", json.data);
+          setData(json.data);
+        });
+   
+
+    }
+
+    
+
   return (
     <>
      
@@ -10,7 +26,7 @@ function PlantCard(props) {
       <div>Name: {plant.name}</div>
       <div>Description: {plant.description}</div>
       <div>Image: {plant.image}</div>
-      <button>Add to Favorties</button>
+      <button onClick={handleFavorite}>Add to Favorties</button>
     </>
   );
 }
